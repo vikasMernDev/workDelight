ffa544f3b76786b016577b7392976287119ca48a
vikasMernDev <vikas.solra@gmail.com>
2023-12-15 11:33:12 +0530

    private routes 2

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Route, Navigate, Outlet } from 'react-router-dom';
 
-const PrivateRoute = ({ path, element: Element, isAuthenticated, ...props }) => {
+const PrivateRoute = () => {
 
 const user = localStorage.getItem('email')
-console.log("564546",user)
 
   return user ? <Outlet /> : <Navigate to="/" replace />
 
